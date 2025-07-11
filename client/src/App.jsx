54e55d93cc9db97b9ee69b4ec3241e6cec73d7fc
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2025-07-11 18:51:31 +0300

    changing logo and fixing modul error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from '/logo.svg'
 import './App.css'
 
 function App() {
@@ -9,9 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        <img src={logo} className="logo" alt="Vite logo" />
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
